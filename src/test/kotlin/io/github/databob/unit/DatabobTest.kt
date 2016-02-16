091c8703b1edf21e0d1085323163e332b74af7a2
David Denton <denton.david@gmail.com>
2016-02-16 22:50:44 +0000

    dave - README

--- src/test/kotlin/io/github/databob/unit/DatabobTest.kt
+++ src/test/kotlin/io/github/databob/unit/DatabobTest.kt
@@ -33,19 +33,7 @@ class DatabobTest {
         assertTrue(default.mk(Exception::class) is Exception)
         assertTrue(default.mk(RuntimeException::class) is RuntimeException)
     }
-/*
-            ZoneId::class.defaultType to { d -> ZoneId.systemDefault() },
-            Date::class.defaultType to { d -> Date() },
-            LocalTime::class.defaultType to { d -> LocalTime.now() },
-            LocalDate::class.defaultType to { d -> LocalDate.now() },
-            LocalDateTime::class.defaultType to { d -> LocalDateTime.of(d.mk(LocalDate::class), d.mk(LocalTime::class)) },
-            ZonedDateTime::class.defaultType to { d -> ZonedDateTime.of(d.mk(LocalDateTime::class), d.mk(ZoneId::class)) },
-            java.sql.Date::class.defaultType to { d -> java.sql.Date(d.mk(Date::class).time) },
-            Timestamp::class.defaultType to { d -> Timestamp.valueOf(d.mk(LocalDateTime::class)) },
-            Duration::class.defaultType to { d -> Duration.ofMillis(d.mk(Long::class)) },
-            Period::class.defaultType to { d -> Period.between(d.mk(LocalDate::class), d.mk(LocalDate::class)) }
 
- */
     @Test
     fun support_date_times() {
 
