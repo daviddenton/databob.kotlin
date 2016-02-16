a6de64bc78197715cdc97df050ec7b9ad9c6708a
David Denton <denton.david@gmail.com>
2016-02-16 20:48:38 +0000

    dave - tidy

--- src/main/kotlin/io/github/databob/CollectionGenerator.kt
+++ src/main/kotlin/io/github/databob/CollectionGenerator.kt
@@ -8,26 +8,22 @@ import kotlin.reflect.jvm.javaType
 
 class CollectionGenerator : Generator {
 
-    private val toSet = { t: List<Any> -> setOf(t[0]) }
-    private val toList = { t: List<Any> -> listOf(t[0]) }
-    private val toMap = { t: List<Any> -> mapOf(t[0] to t[1]) }
-
     private val lookup: Map<Type, (List<Any>) -> Any> = mapOf(
-            Set::class.defaultType.javaType to toSet,
-            List::class.defaultType.javaType to toList,
-            Map::class.defaultType.javaType to toMap
+            Set::class.defaultType.javaType to { t -> setOf(t[0]) },
+            List::class.defaultType.javaType to { t -> listOf(t[0]) },
+            Map::class.defaultType.javaType to { t -> mapOf(t[0] to t[1]) }
     )
 
     override fun get(type: KType, instance: Databob): Any? {
-        if (type.javaType is ParameterizedType) {
-            val coreType = type.javaType as ParameterizedType
-            val key = coreType.rawType ?: ""
-            val map: List<Any> = coreType.actualTypeArguments.map { t ->
-                instance.mk(Class.forName(t.typeName)) ?: ""
+        return when {
+            type.javaType is ParameterizedType -> {
+                val coreType = type.javaType as ParameterizedType
+                val key = coreType.rawType ?: ""
+                return lookup[key]?.invoke(coreType.actualTypeArguments.map { t ->
+                    instance.mk(Class.forName(t.typeName)) ?: ""
+                })
             }
-            return lookup[key]?.invoke(map)
-        } else {
-            return null
+            else -> null
         }
     }
 }
--- src/main/kotlin/io/github/databob/Databob.kt
+++ src/main/kotlin/io/github/databob/Databob.kt
@@ -5,7 +5,7 @@ import kotlin.reflect.defaultType
 
 class Databob {
 
-    private val lookup = CompositeGenerator(PrimitiveGenerator()).with(CollectionGenerator())
+    private val generator = CompositeGenerator(PrimitiveGenerator()).with(CollectionGenerator())
 
     fun <R : Any> mk(c: KClass<R>): R {
         return mk(c.java)
@@ -14,14 +14,14 @@ class Databob {
     fun <R : Any> doIt(c: Class<R>): R {
         val constructor = c.kotlin.constructors.iterator().next()
         val generatedParameters = constructor.parameters
-                .map { k -> lookup.get(k.type, this) ?: mk(Class.forName(k.type.toString())) }
+                .map { k -> generator.get(k.type, this) ?: mk(Class.forName(k.type.toString())) }
                 .map { b -> b!! }
         return constructor.call(*generatedParameters.toTypedArray())
 
     }
 
+    @Suppress("UNCHECKED_CAST")
     fun <R : Any> mk(c: Class<R>): R {
-        val get: Any = lookup.get(c.kotlin.defaultType, this) ?: doIt(c)
-        return get as R
+        return (generator.get(c.kotlin.defaultType, this) ?: doIt(c)) as R
     }
 }
