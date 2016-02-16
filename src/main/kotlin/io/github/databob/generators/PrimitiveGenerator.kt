6c3533d35f92a1377d2b47b172fe059e0c57ea1d
David Denton <denton.david@gmail.com>
2016-02-16 21:44:05 +0000

    dave - more tidy

--- src/main/kotlin/io/github/databob/generators/PrimitiveGenerator.kt
+++ src/main/kotlin/io/github/databob/generators/PrimitiveGenerator.kt
@@ -2,22 +2,30 @@ package io.github.databob.generators
 
 import io.github.databob.Databob
 import io.github.databob.Generator
+import java.math.BigDecimal
+import java.math.BigInteger
 import kotlin.reflect.KType
 import kotlin.reflect.defaultType
 
 class PrimitiveGenerator : Generator {
 
-    private val lookup: Map<KType, () -> Any> = mapOf(
-            Char::class.defaultType to { -> 'c' },
-            Long::class.defaultType to { -> 1L },
-            Float::class.defaultType to { -> 1.0f },
-            Double::class.defaultType to { -> 1.toDouble() },
-            Boolean::class.defaultType to { -> true },
-            String::class.defaultType to { -> "bob" },
-            Int::class.defaultType to { -> 1 }
+    private val lookup: Map<KType, (Databob) -> Any> = mapOf(
+            Double::class.defaultType to { d -> 0.0 },
+            String::class.defaultType to { d -> "bob" },
+            Boolean::class.defaultType to { d -> true },
+            Char::class.defaultType to { d -> d.mk(Int::class).toChar() },
+            Byte::class.defaultType to { d -> d.mk(Int::class).toByte() },
+            Float::class.defaultType to { d -> d.mk(Double::class).toFloat() },
+            Short::class.defaultType to { d -> d.mk(Int::class).toShort() },
+            Long::class.defaultType to { d -> d.mk(Int::class).toLong() },
+            Int::class.defaultType to { d -> d.mk(Double::class).toInt() },
+            BigInteger::class.defaultType to { d -> BigInteger.valueOf(d.mk(Long::class)) },
+            BigDecimal::class.defaultType to { d -> BigDecimal.valueOf(d.mk(Double::class)) },
+            Exception::class.defaultType to { d -> Exception(d.mk(String::class)) },
+            RuntimeException::class.defaultType to { d -> RuntimeException(d.mk(String::class)) }
     )
 
     override fun get(type: KType, databob: Databob): Any? {
-        return lookup[type]?.invoke()
+        return lookup[type]?.invoke(databob)
     }
 }
--- src/test/kotlin/Bob.kt
+++ src/test/kotlin/Bob.kt
@@ -1,4 +1,6 @@
 import io.github.databob.Databob
+import java.math.BigDecimal
+import java.math.BigInteger
 
 data class Bob(val v: String, val b: Rita)
 data class Rita(val v: List<String>)
@@ -6,8 +8,11 @@ data class Mark(val v: Set<Double>)
 data class Bill(val v: Map<String, Float>)
 
 fun main(args: Array<String>) {
+    println(Databob().mk(BigDecimal::class))
+    println(Databob().mk(BigInteger::class))
     println(Databob().mk(Bob::class))
     println(Databob().mk(Mark::class))
     println(Databob().mk(Rita::class))
     println(Databob().mk(Bill::class))
+    println(Databob().mk(RuntimeException::class))
 }
