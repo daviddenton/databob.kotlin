17d77efc263cc1f9578323ec0a3590461ef31324
David Denton <denton.david@gmail.com>
2016-02-21 19:48:25 +0000

    dave - fix readme

--- src/main/kotlin/io/github/databob/generators/PrimitiveGenerator.kt
+++ src/main/kotlin/io/github/databob/generators/PrimitiveGenerator.kt
@@ -5,17 +5,18 @@ import io.github.databob.Generator
 import java.lang.reflect.Type
 import java.math.BigDecimal
 import java.math.BigInteger
+import java.util.*
 import kotlin.reflect.defaultType
 import kotlin.reflect.jvm.javaType
 
 class PrimitiveGenerator : Generator {
 
     private val lookup: Map<Type, (Databob) -> Any> = mapOf(
-            Double::class.defaultType.javaType to { d -> 0.0 },
-            String::class.defaultType.javaType to { d -> "bob" },
-            Boolean::class.defaultType.javaType to { d -> true },
-            Char::class.defaultType.javaType to { d -> d.mk(Int::class).toChar() },
-            Byte::class.defaultType.javaType to { d -> d.mk(Int::class).toByte() },
+            Double::class.defaultType.javaType to { d -> Random().nextDouble() * Int.MAX_VALUE },
+            String::class.defaultType.javaType to { d -> UUID.randomUUID().toString() },
+            Boolean::class.defaultType.javaType to { d -> d.mk(CoinToss::class).toss() },
+            Char::class.defaultType.javaType to { d -> Random().nextInt(256).toChar() },
+            Byte::class.defaultType.javaType to { d -> Random().nextInt(256).toByte() },
             Float::class.defaultType.javaType to { d -> d.mk(Double::class).toFloat() },
             Short::class.defaultType.javaType to { d -> d.mk(Int::class).toShort() },
             Long::class.defaultType.javaType to { d -> d.mk(Int::class).toLong() },
