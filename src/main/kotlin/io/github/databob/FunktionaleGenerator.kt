6facd4f1904e669ba929258506d1d7f223420e7a
David Denton <denton.david@gmail.com>
2016-08-24 09:53:28 +0100

    made funktionale dependency properly provided

--- src/main/kotlin/io/github/databob/FunktionaleGenerator.kt
+++ src/main/kotlin/io/github/databob/FunktionaleGenerator.kt
@@ -10,13 +10,15 @@ import kotlin.reflect.jvm.javaType
 
 class FunktionaleGenerator : io.github.databob.Generator {
 
-    private val lookup: Map<java.lang.reflect.Type, (Array<java.lang.reflect.Type>, io.github.databob.Databob) -> Any> = mapOf(
-            Option::class.defaultType.javaType to { t, d -> d.mk(Class.forName(t[0].typeName)).toOption().filter { t -> d.mk<CoinToss>().toss() } },
-            Either::class.defaultType.javaType to { t, d ->
-                val pair = Pair(d.mk(Class.forName(t[0].typeName)), d.mk(Class.forName(t[1].typeName)))
-                if(d.mk<CoinToss>().toss()) pair.toRight() else pair.toLeft()
-            }
-    )
+    private val lookup by lazy {
+        mapOf<java.lang.reflect.Type, (Array<java.lang.reflect.Type>, io.github.databob.Databob) -> Any>(
+                Option::class.defaultType.javaType to { t, d -> d.mk(Class.forName(t[0].typeName)).toOption().filter { t -> d.mk<CoinToss>().toss() } },
+                Either::class.defaultType.javaType to { t, d ->
+                    val pair = Pair(d.mk(Class.forName(t[0].typeName)), d.mk(Class.forName(t[1].typeName)))
+                    if (d.mk<CoinToss>().toss()) pair.toRight() else pair.toLeft()
+                }
+        )
+    }
 
     override fun mk(type: java.lang.reflect.Type, databob: io.github.databob.Databob): Any? = when (type) {
         is java.lang.reflect.ParameterizedType -> {
