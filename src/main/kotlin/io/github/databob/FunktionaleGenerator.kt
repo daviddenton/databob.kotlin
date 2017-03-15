5786bd33dcd6e29cb4f06af09938ebd6a0b4daee
David Denton <denton.david@gmail.com>
2017-03-15 11:31:11 +0000

    tidy

--- src/main/kotlin/io/github/databob/FunktionaleGenerator.kt
+++ src/main/kotlin/io/github/databob/FunktionaleGenerator.kt
@@ -5,13 +5,15 @@ import org.funktionale.either.toLeft
 import org.funktionale.either.toRight
 import org.funktionale.option.Option
 import org.funktionale.option.toOption
+import java.lang.reflect.ParameterizedType
+import java.lang.reflect.Type
 import kotlin.reflect.defaultType
 import kotlin.reflect.jvm.javaType
 
 class FunktionaleGenerator : io.github.databob.Generator {
 
     private val lookup by lazy {
-        mapOf<java.lang.reflect.Type, (Array<java.lang.reflect.Type>, io.github.databob.Databob) -> Any>(
+        mapOf<Type, (Array<Type>, Databob) -> Any>(
                 Option::class.defaultType.javaType to { t, d -> d.mk(Class.forName(t[0].typeName)).toOption().filter { t -> d.mk<CoinToss>().toss() } },
                 Either::class.defaultType.javaType to { t, d ->
                     val pair = Pair(d.mk(Class.forName(t[0].typeName)), d.mk(Class.forName(t[1].typeName)))
@@ -20,8 +22,8 @@ class FunktionaleGenerator : io.github.databob.Generator {
         )
     }
 
-    override fun mk(type: java.lang.reflect.Type, databob: io.github.databob.Databob): Any? = when (type) {
-        is java.lang.reflect.ParameterizedType -> {
+    override fun mk(type: Type, databob: Databob): Any? = when (type) {
+        is ParameterizedType -> {
             lookup[type.rawType]?.invoke(type.actualTypeArguments, databob)
         }
         else -> null
